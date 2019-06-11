f96da5d609525f53835e7734c16b139c018b2a20
Matt <matthaag11@gmail.com>
2019-06-11 12:59:39 -0500

    returns a name and age, without any error handling

--- helloworld/handler.go
+++ helloworld/handler.go
@@ -3,18 +3,29 @@ package function
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/openfaas-incubator/go-function-sdk"
 )
 
 // Handle a function invocation
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
-	message := fmt.Sprintf("%s was here!", string(req.Body))
+	name, age := determineAge(string(req.Body))
+	message := fmt.Sprintf("%s is %d years old", string(req.Body))
 
 	return handler.Response{
 		Body:       []byte(message),
 		StatusCode: http.StatusOK,
 	}, err
+}
+
+func determineAge(name string) (string, int) {
+
+	a := make(map[string]int)
+
+	a["Matt"] = 28
+	a["Jordan"] = 28
+	a["Trevor"] = 27
+	a["Tom"] = 31
+
+	return name, a[name]
 }
